521ca665f1e7e0ec5c67a686fcff8128d095ecb6
Ajaya.Viyyapu <Ajaya.Viyyapu@nox-nachtexpress.de>
2026-01-28 00:00:22 +0100

    more assignments

--- PythonAssignments/AjayPackage/string.py
+++ PythonAssignments/AjayPackage/string.py
@@ -1,7 +1,7 @@
 import re
 
 def delete_special_characters(sentence) :
-    match_exp = r'[^A-Za-z ]'
+    match_exp = r'[^A-Za-z0-9 ]'
     special_symbols = re.findall(match_exp,sentence)
     for symbol in special_symbols :
         sentence_without_special_symbols = sentence.replace(symbol,'')
