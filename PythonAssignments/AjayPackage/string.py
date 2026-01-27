237580b85c730e0035393b62b9c43eb62f9d8d66
Ajaya.Viyyapu <Ajaya.Viyyapu@nox-nachtexpress.de>
2026-01-27 22:54:24 +0100

    Learning packages in python

---
+++ PythonAssignments/AjayPackage/string.py
@@ -0,0 +1,9 @@
+import re
+
+def delete_special_characters(sentence) :
+    match_exp = r'[^A-Za-z ]'
+    special_symbols = re.findall(match_exp,sentence)
+    for symbol in special_symbols :
+        sentence_without_special_symbols = sentence.replace(symbol,'')
+        sentence = sentence_without_special_symbols
+    return sentence_without_special_symbols
